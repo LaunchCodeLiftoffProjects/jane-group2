fa59d603b9618da7e31d44df8a1bb3c7b3c3fcb0
jmbun <joelbrooks.us@gmail.com>
2022-01-06 19:03:30 -0600

    Fix login form

--- src/main/java/org/boxproject/models/dto/LoginFormDTO.java
+++ src/main/java/org/boxproject/models/dto/LoginFormDTO.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class LoginFormDTO {
+
     @NotNull
     @NotBlank
     @Size(min = 3, max = 20, message = "Invalid username. Must be between 3 and 20 characters.")
@@ -30,4 +31,5 @@ public class LoginFormDTO {
     public void setPassword(String password) {
         this.password = password;
     }
+
 }
