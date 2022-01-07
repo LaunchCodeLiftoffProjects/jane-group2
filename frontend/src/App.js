22b886e43a73e440188a1fb78d4dbc8400bd47f2
VampyVicious <vampynguyen@hotmail.com>
2022-01-06 18:52:50 -0600

    fixed syntax error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,84 +1,95 @@
-import React, {
-    useEffect,
-    useState
-} from 'react';
+import React, { useEffect, useState } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
 import {
-    useNavigate,
-    Outlet
-} from 'react-router-dom';
-import { Container, Navbar, NavbarBrand, NavLink, Button } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap'
-import './App.css';
-import { authService } from "./util/auth"
+  Container,
+  Navbar,
+  NavbarBrand,
+  NavLink,
+  Button,
+} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./App.css";
+import { authService } from "./util/auth";
 import Search from "./search";
 
 export default function App() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        let subscription = authService.currentUser.subscribe((user) => {
-            setUser(user);
-        });
-        return () => {
-            subscription.unsubscribe();
-        };
-    }, [user]);
+  useEffect(() => {
+    let subscription = authService.currentUser.subscribe((user) => {
+      setUser(user);
+    });
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [user]);
 
-    const logout = () => {
-        authService.logout();
-        navigate("/", { replace: true });
-    }
+  const logout = () => {
+    authService.logout();
+    navigate("/", { replace: true });
+  };
 
-    return (
-        <div className='centered'>
-            <header>
-                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-                    <Container>
-                        <NavbarBrand to="/" className="header-top">
-                            <h1>Piece of Mind</h1>
-                        </NavbarBrand>
-                        <ul className="navbar-nav flex-grow">
-                            <LinkContainer to="/">
-                                <NavLink className="text-dark" to="/">Home</NavLink>
-                            </LinkContainer>
-                            {!user &&
-                                // if we are not logged in
-                                <div>
-                                    <LinkContainer to="/login">
-                                        <NavLink className="text-dark" to="/login">Login</NavLink>
-                                    </LinkContainer>
-                                </div>
-                            }
-                            {user &&
-                                // if we are logged in
-                                <div>
-                                    <LinkContainer to="/testAuth">
-                                        <NavLink className="text-dark" to="/testAuth">VIP Page</NavLink>
-                                    </LinkContainer>
-                                </div>
-                            }
-                        </ul>
-                        {user &&
-                            // if we are logged in
-                            <div>
-                                <Button onClick={logout} style={{ float: "right" }}>Logout</Button>
-                            </div>
-                        }
-                    </Container>
-                </Navbar>
-            </header>
-            <hr
-              style={{
-                border: "0",
-                height: "1px",
-                backgroundImage:
-                  "linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0))",
-              }}
-            />
-            <Search />
-            <Outlet />
-        </div>
-    }
+  return (
+    <div className="centered">
+      <header>
+        <Navbar
+          className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3"
+          light
+        >
+          <Container>
+            <NavbarBrand to="/" className="header-top">
+              <h1>Piece of Mind</h1>
+            </NavbarBrand>
+            <ul className="navbar-nav flex-grow">
+              <LinkContainer to="/">
+                <NavLink className="text-dark" to="/">
+                  Home
+                </NavLink>
+              </LinkContainer>
+              {!user && (
+                // if we are not logged in
+                <div>
+                  <LinkContainer to="/login">
+                    <NavLink className="text-dark" to="/login">
+                      Login
+                    </NavLink>
+                  </LinkContainer>
+                </div>
+              )}
+              {user && (
+                // if we are logged in
+                <div>
+                  <LinkContainer to="/testAuth">
+                    <NavLink className="text-dark" to="/testAuth">
+                      VIP Page
+                    </NavLink>
+                  </LinkContainer>
+                </div>
+              )}
+            </ul>
+            {user && (
+              // if we are logged in
+              <div>
+                <Button onClick={logout} style={{ float: "right" }}>
+                  Logout
+                </Button>
+              </div>
+            )}
+          </Container>
+        </Navbar>
+      </header>
+      <hr
+        style={{
+          border: "0",
+          height: "1px",
+          backgroundImage:
+            "linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0))",
+        }}
+      />
+      <Search />
+      <Outlet />
+    </div>
+  );
 }
