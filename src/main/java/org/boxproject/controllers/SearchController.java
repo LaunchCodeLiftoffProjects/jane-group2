39850044e325a069220b7e4989f72bd1168c4d7b
VampyVicious <vampynguyen@hotmail.com>
2022-01-20 20:01:35 -0600

    search controller completed

--- src/main/java/org/boxproject/controllers/SearchController.java
+++ src/main/java/org/boxproject/controllers/SearchController.java
@@ -2,12 +2,20 @@ package org.boxproject.controllers;
 
 import org.boxproject.models.Box;
 import org.boxproject.models.BoxItem;
+import org.boxproject.models.BoxUser;
 import org.boxproject.models.data.BoxItemRepository;
+import org.boxproject.models.data.BoxUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/search")
@@ -16,26 +24,43 @@ public class SearchController {
         @Autowired
         private BoxItemRepository boxItemRepository;
 
-        @RequestMapping("")
-        public String search() {
-                return List<BoxItem>;
-                return boxItemRepository.findAll();
-        }
+        @Autowired
+        private BoxItem boxItem;
+
+        @Autowired
+        private BoxUserRepository boxUserRepository;
 
         @PostMapping("Results")
         public String searchResults() {
-                return "searchResults";
+                return "Search Results: ";
         }
-}
-
-
-/*@GetMapping
-public Iterable<Box> retrieveBoxes() throws Exception {
+        
+        @GetMapping
+        public Iterable<Box> searchBoxes (String Search) throws Exception {
         final BoxUser boxUser = getBoxUser();
-        System.out.println("User: " + boxUser);
+        List<Box> boxes = new ArrayList<Box>();
+        //System.out.println("Search Results: " + boxSearch);
         for(Box box : boxUser.getBoxes()) {
-            System.out.println("User owns box: " + box);
+            System.out.println("Items in box: " + box);
+            boolean doesBoxContainSearch = false;
+            for(BoxItem item : box.getBoxItems()) {
+                if (item.getItemName().contains(Search)) {
+                    doesBoxContainSearch = true;
+                }
+            }
+            if (doesBoxContainSearch) {
+                boxes.add(box);
+            }
         }
-        return boxUser.getBoxes();
+        return boxes;
     }
-    */
+
+    @GetMapping("{boxId}")
+    private BoxUser getBoxUser() throws Exception {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            throw new Exception("User is not authorized!");
+        }
+        return boxUserRepository.findByUsername(authentication.getName());
+    }
+}
