909c4cf12e6d3836de2c45daf7c1a3f8fc22102f
VampyVicious <vampynguyen@hotmail.com>
2022-01-27 19:17:35 -0600

    edited more of searchpageresults

---
+++ frontend/src/components/searchPageResults.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+import { createBox, getAllBoxes } from '../services/boxService';
+
+export default function BoxList() {
+
+    // Boxes are appended to a list by clicking on an add button.
+    // Each box should be a button that links to it's own route through an ID.
+
+    const [boxList, setBoxList] = useState([]);
+    const [labelName, setLabelName] = useState('');
+
+    async function updateBoxList() {
+        setBoxList(await getAllBoxes());
+    }
+
+    useEffect(() => {
+        updateBoxList();
+    }, []);
+
+    const handleChange = event => {
+        setLabelName(event.target.value);
+    };
+
+    const handleAdd = async (event) => {
+        event.preventDefault();
+
+        createBox({ labelName }).then(response => {
+            setLabelName('');
+            updateBoxList();
+        });
+    };
+
+    return (
+        <div className="container">
+            <form className="d-flex justify-content-center m-3" onSubmit={handleAdd}>
+
+                <div className="input-group w-25">
+                    <input className="form-control" type="text" value={labelName} onChange={handleChange} />
+                    <button className="btn btn-dark" type="submit" onClick={handleAdd}>Add Box</button>
+                </div>
+
+            </form>
+
+            {(boxList && boxList.length > 0) ?
+                <ul>
+                    {boxList.map(box => (
+                        <li className="card" style={{ listStyle: "none" }} key={box.id}>
+                            <Link
+                                to={{
+                                    pathname: `/boxDisplay/${box.id}`,
+                                    state: { labelName }
+                                }}
+                            >
+                                {box.labelName}
+                            </Link>
+                        </li>
+                    ))}
+                </ul> :
+                <h3 className="mt-5">You currently have no boxes to show. Add boxes above to view list.</h3>
+            }
+        </div>
+    );
+}
---
+++ frontend/src/services/searchService.js
@@ -0,0 +1,31 @@
+import { authService } from "../services/auth";
+
+export async function searchAllBoxes() {
+
+    try {
+        return await fetch('/api/boxes', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': authService.header()
+            }
+        })
+            .then(response => response.json())
+            .then(json => {
+                let boxes = [];
+                for (let box of json) {
+                    boxes.push({
+                        "id": box.id,
+                        "labelName": box.labelName,
+                        "labelColor": box.labelColor,
+                    });
+                }
+                return boxes;
+            });
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+
+}
+}
